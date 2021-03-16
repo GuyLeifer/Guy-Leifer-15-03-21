084b15bcd1a8a3391d05d21869987f628feb849d
GuyLeifer <67858228+GuyLeifer@users.noreply.github.com>
2021-03-16 16:52:19 +0200

    Update Product.js

--- src/components/items/addItem/products/Product.js
+++ src/components/items/addItem/products/Product.js
@@ -1,8 +1,7 @@
 import './Product.css';
 function Product({ product, productSetter }) {
     return (
-        // <div className="product" onClick={() => productSetter(product)}>
-        <>
+        <div className="product" onClick={() => productSetter(product)}>
             <div className="top-line">
                 <div className="product-title">{product.title}</div>
                 <div className="product-description">{product.description}</div>
@@ -12,8 +11,7 @@ function Product({ product, productSetter }) {
                 <div className="product-price">Price: {product.price}</div>
                 <div className="product-category">Category: {product.category}</div>
             </div>
-        </>
-        // </div>
+        </div>
     )
 }
 
