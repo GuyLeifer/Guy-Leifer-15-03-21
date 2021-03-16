e02894c7e6f142bcae8a7fb84110feb1eb1bb829
GuyLeifer <67858228+GuyLeifer@users.noreply.github.com>
2021-03-16 16:54:18 +0200

    Store component

--- src/components/stores/Store.js
+++ src/components/stores/Store.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import TableMain from '../table/TableMain';
+import { itemsState } from '../../Atoms/itemsState';
+import { useRecoilValue } from "recoil";
 
 function Store() {
+
+    const items = useRecoilValue(itemsState);
+    let itemsByStore = [];
+
+    items.forEach(item => {
+        const index = itemsByStore.findIndex(itemByStore => itemByStore.store === item.store);
+        if (index !== -1) {
+            itemsByStore[index].price += item.price;
+            itemsByStore[index].quentity++;
+            itemsByStore[index].items = [...itemsByStore[index].items, item];
+        } else {
+            itemsByStore.push(
+                {
+                    store: item.store,
+                    price: item.price,
+                    quentity: 1,
+                    items: [item]
+                }
+            )
+        }
+    });
+
     return (
         <div>
-
+            <TableMain items={itemsByStore} type="Store" />
         </div>
     )
 }
