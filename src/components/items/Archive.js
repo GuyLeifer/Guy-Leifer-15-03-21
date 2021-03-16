54ac0272f471fba20263be3a2ca8529b3ae48348
GuyLeifer <67858228+GuyLeifer@users.noreply.github.com>
2021-03-16 16:52:32 +0200

    Archive component

--- src/components/items/Archive.js
+++ src/components/items/Archive.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import TableMain from '../table/TableMain';
+
+import { itemsState, autoFilterState, archiveItemsState } from '../../Atoms/itemsState';
+import { useRecoilValue } from "recoil";
 
 function Archive() {
+
+    const items = useRecoilValue(itemsState)
+    const autoFilter = useRecoilValue(autoFilterState)
+    const autoFilterItems = items.filter(item => item.deliveryEstimate < new Date())
+
+    const archiveItems = useRecoilValue(archiveItemsState)
+
     return (
         <div>
-
+            <TableMain type="Archive" autoFilter={autoFilter} items={autoFilter ? autoFilterItems : archiveItems} />
         </div>
     )
 }
